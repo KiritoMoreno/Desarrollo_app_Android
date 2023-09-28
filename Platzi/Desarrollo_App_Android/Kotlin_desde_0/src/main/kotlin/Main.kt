c9ed0312c28eb942f941717d75f0baf2374210eb
kiritomoreno <moreno.algorithmictrading@gmail.com>
2023-09-28 20:02:26 +0100

    Kotlin desde cero

---
+++ Platzi/Desarrollo_App_Android/Kotlin_desde_0/src/main/kotlin/Main.kt
@@ -0,0 +1,35 @@
+const val PI = 3.1416
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    var dinero = 30
+    println(dinero)
+    dinero = 5
+    println(dinero)
+
+    val nombre = "Moreno"
+    println(nombre)
+    println(PI)
+
+    val booleanVar= true
+    val numeroLargo = 3L
+    val doubleNum = 2.7182
+    val floatNum = 1.1f
+
+    val primerValor = 20
+    var segundoValor = 10
+
+    val tercerValor = primerValor - segundoValor
+    println(tercerValor)
+
+    // Cadenas de texto
+    val apellido = "Moreno"
+    val name = "Andres"
+
+    val nombreCompleto = "Mi nombre es $name $apellido"
+    println(nombreCompleto)
+
+
+
+
+}
