233f398d50f0aca0c27e72b2f022e2e185e1a00a
kiritomoreno <moreno.algorithmictrading@gmail.com>
2023-09-28 20:49:10 +0100

    Bucles: While y Do While

--- Platzi/Desarrollo_App_Android/Kotlin_desde_0/src/main/kotlin/Main.kt
+++ Platzi/Desarrollo_App_Android/Kotlin_desde_0/src/main/kotlin/Main.kt
@@ -1,28 +1,14 @@
 const val PI = 3.1416
 fun main(args: Array<String>) {
-    val nombreColor = "Amarillo"
-
-    when(nombreColor){
-        "Amarillo" -> println("El amarillo es el color del sol")
-        "Rojo","Carmesi" -> println("El rojo es el color del fuego") // Puedo poner dos opciones y  utilizamos el mismo mensaje
-        else -> println("Error, no tengo información del color")
+    var contador = 10
+    while(contador > 0){
+        println("El valor del contador es $contador")
+        contador--
     }
 
-    val code = 200
-    // (in) quiere decir que se encuentra en
-    // (..) quiere decir desde y hasta segun yo xD
-    when(code){
-        in 200..299 -> println("Todo ha ido bien")
-        in 400..500 -> println("Algo ha fallado")
-        else -> println("Codigo desconocido, algo ha fallado")
-    }
-
-    val tallaDeZapatos = 45
-    val mensaje = when(tallaDeZapatos){
-        41,43 -> "Tenemos disponible"
-        42,44 -> "Casi no nos quedan"
-        45 -> "Lo siento no tenemos disponible"
-        else -> "Estos zapatos solo vienen en tallas 41, 42, 43, 44 y 45"
-    }
-    println(mensaje)
+    do{
+        println("Generando numero aleatorio...")
+        val numeroAleatorio = (0..100).random()
+        println("El numero generado es $numeroAleatorio")
+    }while(numeroAleatorio>50)
 }
