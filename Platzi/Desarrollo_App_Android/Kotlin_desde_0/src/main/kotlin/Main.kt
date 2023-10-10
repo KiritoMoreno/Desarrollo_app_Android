8fc566b3991aab7823eda60cf16b49e2e382fdf5
kiritomoreno <moreno.algorithmictrading@gmail.com>
2023-10-10 22:02:19 +0100

    Diferente tipos de listas

--- Platzi/Desarrollo_App_Android/Kotlin_desde_0/src/main/kotlin/Main.kt
+++ Platzi/Desarrollo_App_Android/Kotlin_desde_0/src/main/kotlin/Main.kt
@@ -1,10 +1,36 @@
 
 fun main(args: Array<String>) {
-    // Elvis operator nos ayudara a devolver un valor
-    var nombre : String?= null
+    // Diferentes tipos de Listas
+    val listaDeNombre= listOf<String>("Juan", "Enrique", "Camila")
+    println(listaDeNombre)
 
-    // Esto nos va dar un error porque estamos intentando asignar un nulable a un tipo entero que no es nulable
-    // Para poder convertirlo vamos a utilizar el operador elvis ( ?: ) y en el lado derecho el valor
-    val caracteresDeNombre : Int = nombre?.length ?: 0
-    println(caracteresDeNombre)
+    // Lista mutable
+    val listaVacia= mutableListOf<String>()
+    println(listaVacia)
+
+    listaVacia.add("Juan")
+    println(listaVacia)
+
+    // Obtener un valor de la lista
+    val valorUsandoGet = listaVacia.get(0)
+    println(valorUsandoGet)
+
+    val valorUsandoOperador = listaVacia[0]
+    println(valorUsandoOperador)
+
+    val primerValor= listaDeNombre.firstOrNull()
+    println(primerValor)
+
+    // Eliminar elemento de una lista
+    listaVacia.removeAt(0)
+    println(listaVacia)
+
+    listaVacia.add("Enrique")
+    println(listaVacia)
+    listaVacia.removeIf{caracteres -> caracteres.length > 3}
+    println(listaVacia)
+
+    //Concepto de arrays en kotlin-> solamente si estamos usando codigo con miles de datos
+    val myArray = arrayOf(1,2,3,4)
+    println("Array como lista ${myArray.toList()}")
 }
