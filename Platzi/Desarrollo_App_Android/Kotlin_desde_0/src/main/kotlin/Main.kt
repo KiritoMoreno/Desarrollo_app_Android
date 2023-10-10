d2530d27ce725d33ba6c42d1fac105d7b6619fdc
kiritomoreno <moreno.algorithmictrading@gmail.com>
2023-10-10 10:17:06 +0100

    Elvis operator

--- Platzi/Desarrollo_App_Android/Kotlin_desde_0/src/main/kotlin/Main.kt
+++ Platzi/Desarrollo_App_Android/Kotlin_desde_0/src/main/kotlin/Main.kt
@@ -1,23 +1,10 @@
 
 fun main(args: Array<String>) {
-    // Try Catch
+    // Elvis operator nos ayudara a devolver un valor
     var nombre : String?= null
-    // Intentamos entrar a la variable nombre cuando es nulo
-    try {
-        nombre!!.length
-    }catch (excepcion: NullPointerException){
-        println("Ha ocurrido un error")
-    } finally {
-        println("Finalmente ha ocurrido un error.. Cerrando aplicación")
-    }
 
-    //Podemos capturar este error
-    //Asignamos un valor 0
-    val primerValor = 10
-    val segundoValor = 0
-    val resultado : Int = try {
-        primerValor/segundoValor
-    }catch (exception : Exception){0}
-    println(resultado)
-
+    // Esto nos va dar un error porque estamos intentando asignar un nulable a un tipo entero que no es nulable
+    // Para poder convertirlo vamos a utilizar el operador elvis ( ?: ) y en el lado derecho el valor
+    val caracteresDeNombre : Int = nombre?.length ?: 0
+    println(caracteresDeNombre)
 }
