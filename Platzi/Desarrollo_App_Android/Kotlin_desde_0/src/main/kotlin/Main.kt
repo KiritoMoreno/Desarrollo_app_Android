b84585b9f0872cf9a1174c8fbb18c9bb2f7dfd46
kiritomoreno <moreno.algorithmictrading@gmail.com>
2023-10-10 22:37:26 +0100

    Sets

--- Platzi/Desarrollo_App_Android/Kotlin_desde_0/src/main/kotlin/Main.kt
+++ Platzi/Desarrollo_App_Android/Kotlin_desde_0/src/main/kotlin/Main.kt
@@ -1,38 +1,22 @@
 
 fun main(args: Array<String>) {
-    // Maps -> Son elementos de clave valor.
-    // Nos permite ordenar los elementos de una forma que tenemos una clave que tiene asignado un valor
+    // Sets -> No pueden tener elementos repetidos
 
-    // Mapa Inmutables
-    val edadSuperHeroes = mapOf(
-        "Ironman" to 35,
-        "Spiderman" to 23,
-        "Capitan America" to 99
-    ) // se accede mediante las keys
-    println(edadSuperHeroes)
+    // Set Inmutable
+    val vocalesRepetidas = setOf("a","e","i","o","u","a","e","i","o","u")
+    println(vocalesRepetidas) // No imprime los elementos repetidos
 
-    //Mapa Mutable -> Permite que se modifiquen elementos
-    val edadSuperHeroesMutable = mutableMapOf(
-        "Ironman" to 35,
-        "Spiderman" to 23,
-        "Capitan America" to 99
-    )
-    println(edadSuperHeroesMutable)
-    //Agregamos
-    edadSuperHeroesMutable.put("Wolverine",45)
-    println(edadSuperHeroesMutable)
+    // Set Mutable
+    val numerosFavoritos = mutableSetOf(1,2,3,4)
+    println(numerosFavoritos)
+    numerosFavoritos.add(5)
+    println(numerosFavoritos)
 
-    edadSuperHeroesMutable["Storm"] = 30
-    println(edadSuperHeroesMutable)
+    // Eliminamos pasandole el valor que queremos quitar
+    numerosFavoritos.remove(5)
+    println(numerosFavoritos)
 
-    val edadIronman = edadSuperHeroesMutable["Ironman"]
-    println(edadIronman)
-
-    // Eliminar elementos de nuestro mapa
-    edadSuperHeroesMutable.remove("Wolverine")
-    println(edadSuperHeroesMutable)
-
-    // Conocer todas las claves y valores
-    println(edadSuperHeroesMutable.keys)
-    println(edadSuperHeroesMutable.values)
+    // Podemos obtener un elemento de esta forma,en base de una condición
+    val valorDelSet: Int? = numerosFavoritos.firstOrNull{ numero -> numero > 2}
+    println(valorDelSet)
 }
