2f92caee57a631e4342407e729b4a9b989a0de72
kiritomoreno <moreno.algorithmictrading@gmail.com>
2023-10-13 19:20:04 +0100

    Contestando aleatoriamente

--- Platzi/Desarrollo_App_Android/Kotlin_desde_0/src/main/kotlin/Main.kt
+++ Platzi/Desarrollo_App_Android/Kotlin_desde_0/src/main/kotlin/Main.kt
@@ -10,7 +10,7 @@ val answers = mapOf(
     "Totally" to AFFIRMATIVE_ANSWER,
     "Undoubtedly" to AFFIRMATIVE_ANSWER,
     "Ask in an other moment" to DOUBTFUL_ANSWER,
-    "I can't say you in this moment" to DOUBTFUL_ANSWER,
+    "I can't say at the moment" to DOUBTFUL_ANSWER,
     "Maybe yes, or maybe not" to DOUBTFUL_ANSWER,
     "It's not going to happen" to NEGATIVE_ANSWER,
     "Do not count on it" to NEGATIVE_ANSWER,
@@ -34,12 +34,50 @@ fun main(args: Array<String>) {
         else-> showError()
     }
 }
-
-fun checkAnswer() {
-    TODO("Not yet implemented")
+fun showError(){
+    println("Parece que has elegido una opción no valida")
 }
-
+fun exit(){
+    println("Hasta luego!")
+}
 fun askQuestion() {
-    TODO("Not yet implemented")
+    println("Which Question do you want do to check")
+    readLine()
+    println("umm that was your question... the answer to this is:")
+    val answerGenerada = answers.keys.random()
+    println(answerGenerada)
 }
+fun checkAnswer() {
+    println("Selecciona una opción")
+    println("1. Check all the answers")
+    println("2. Check only affirmative answers")
+    println("3. Check only doubtful answers")
+    println("4. Check only negative answers")
+
+    val opcionIngresada = readLine()
+    when(opcionIngresada){
+        "1" -> mostrarRespuestasPorTipo()
+        "2" -> mostrarRespuestasPorTipo(tipoDeRespuesta = AFFIRMATIVE_ANSWER)
+        "3" -> mostrarRespuestasPorTipo(tipoDeRespuesta = DOUBTFUL_ANSWER)
+        "4" -> mostrarRespuestasPorTipo(tipoDeRespuesta = NEGATIVE_ANSWER)
+        else -> println("Respuesta no valida. Adios.")
+    }
+
+}
+
+fun mostrarRespuestasPorTipo(tipoDeRespuesta : String = "TODOS") {
+    when(tipoDeRespuesta){
+        "TODOS" -> answers.keys.forEach { respuesta -> println(respuesta) }
+        AFFIRMATIVE_ANSWER -> answers.filterValues { values -> values == AFFIRMATIVE_ANSWER }
+            .also { respuestasPositivas -> println(respuestasPositivas.keys) }
+
+        DOUBTFUL_ANSWER -> answers.filterValues { values -> values == DOUBTFUL_ANSWER }
+            .also { respuestasDudosas -> println(respuestasDudosas.keys) }
+
+        NEGATIVE_ANSWER -> answers.filterValues { values -> values == NEGATIVE_ANSWER }
+            .also { respuestasNegativas -> println(respuestasNegativas.keys) }
+    }
+}
+
+
 
