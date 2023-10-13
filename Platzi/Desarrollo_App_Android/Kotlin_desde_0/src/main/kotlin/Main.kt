5a1e92f1fb110537708de1347511a39c41ba7c90
kiritomoreno <moreno.algorithmictrading@gmail.com>
2023-10-13 17:55:19 +0100

    Also

--- Platzi/Desarrollo_App_Android/Kotlin_desde_0/src/main/kotlin/Main.kt
+++ Platzi/Desarrollo_App_Android/Kotlin_desde_0/src/main/kotlin/Main.kt
@@ -1,17 +1,9 @@
 
 fun main(args: Array<String>) {
-    // Apply -> Nos permite realizar operaciones sobre una variable y luego delvolver el valor
-    // Tambien si lo combinamos con la funcion safecall,crea una funcion Apply segura
-    val moviles = mutableListOf("Google Pixel 2XL", "Google Pixel 4a","Iphone","Asus").apply {
-        removeIf { movil -> movil.contains("Google") }
-    }
+    // Also -> Nos permite obtener una variable,ejecutar un codigo con esa variable y luego devolverla como parametro para que pueda ser utilizada por una función más adelante.
+    val moviles = mutableListOf("IOS","Linux","Windows").also {
+        lista -> println("El valor original de la lista es $lista")
+    }.asReversed()
     println(moviles)
-
-    // Esto nos ayuda a evitar a estar entrando a propiedades o modificando variables en el caso de que sean nulables ->
-    val colores : MutableList<String> = mutableListOf("Amarillo","Azul","Rojo")
-    colores?.apply{
-        println("Nuestros colores son $this")
-        println("La cantidad de colores es $size")
-    }
 
 }
