5076b84b87477e29687b768a311097869bdd5fc5
kiritomoreno <moreno.algorithmictrading@gmail.com>
2023-10-13 17:39:31 +0100

    Run

--- Platzi/Desarrollo_App_Android/Kotlin_desde_0/src/main/kotlin/Main.kt
+++ Platzi/Desarrollo_App_Android/Kotlin_desde_0/src/main/kotlin/Main.kt
@@ -1,10 +1,12 @@
 
 fun main(args: Array<String>) {
-    // With nos permite acceder directamente a las propiedades de una variable que le pasamos por parámetro
-    // De esta forma evitamos llamar la variable en varias lineas
-    val colores = listOf("Azul", "Amarillo","Rojo")
-    with(colores){
-        println("Nuestros colores son $this")
-        println("Esta lista tiene una cantidad de colores de $size") // Si queremos acceder a las propiedad de colores,lo podemos hacer directamente con size
-    }
+    // Run
+    val moviles = mutableListOf("Google Pixel 2XL","IOS","Linux","Windows","Google Pixel 4a")
+        .run{
+            removeIf{
+                movil -> movil.contains("Google")
+            }
+            this
+        }
+    println(moviles)
 }
